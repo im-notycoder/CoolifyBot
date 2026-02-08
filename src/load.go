0e143028042b604bcdc9d96345009b3fdcdfbbb5
ɪᴍ-ɴᴏᴛʏᴄᴏᴅᴇʀ <130166544+im-notycoder@users.noreply.github.com>
2026-02-08 19:27:26 +0530

    Refactor load.go and update dispatcher setup
    Refactor load.go to remove unused imports and restructure dispatcher initialization.

--- src/load.go
+++ src/load.go
@@ -1,56 +1,46 @@
 package src
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/PaulSonOfLars/gotgbot/v2"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/callbackquery"
-	"html"
-	"log"
 	"time"
+
+	"coolifymanager/src/scheduler"
+
+	"github.com/AshokShau/gotdbot/ext"
+	"github.com/AshokShau/gotdbot/ext/handlers"
+	"github.com/AshokShau/gotdbot/ext/handlers/filters/callbackquery"
 )
-
-func errorHandler(bot *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
-	var msg string
-	if ctx.Update != nil {
-		if updateBytes, err := json.MarshalIndent(ctx.Update, "", "  "); err == nil {
-			msg = fmt.Sprintf("%s", html.EscapeString(string(updateBytes)))
-		} else {
-			msg = "failed to marshal update"
-		}
-	} else {
-
-		msg = "no update"
-	}
-
-	message := fmt.Sprintf("<blockquote expandable>New Error:\n%s\n\n%s</blockquote>", err.Error(), msg)
-	if _, err = bot.SendMessage(5938660179, message, &gotgbot.SendMessageOpts{ParseMode: "HTML", DisableNotification: true}); err != nil {
-		log.Printf("failed to send error message to logger: %s", err)
-		return ext.DispatcherActionNoop
-	}
-
-	return ext.DispatcherActionNoop
-}
 
 var (
-	startTime  = time.Now()
-	Dispatcher = newDispatcher()
+	startTime = time.Now()
 )
 
-func newDispatcher() *ext.Dispatcher {
-	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{Error: errorHandler, MaxRoutines: 50})
-	dispatcher.AddHandler(handlers.NewCommand("start", startHandler))
-	dispatcher.AddHandler(handlers.NewCommand("ping", PingCommandHandler))
+func InitFunc(d *ext.Dispatcher) error {
+	if err := scheduler.Start(); err != nil {
+		return fmt.Errorf("scheduler start error: %s", err.Error())
+	}
 
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("list_projects"), listProjectsHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("project_menu:"), projectMenuHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("restart:"), restartHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("deploy:"), deployHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("logs:"), logsHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("status:"), statusHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("stop:"), stopHandler))
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("delete:"), deleteHandler))
-	return dispatcher
+	// Commands
+	d.AddHandler(handlers.NewCommand("start", startHandler))
+	d.AddHandler(handlers.NewCommand("ping", pingHandler))
+	d.AddHandler(handlers.NewCommand("jobs", jobsHandler))
+	d.AddHandler(handlers.NewCommand("job", scheduleHandler))
+	d.AddHandler(handlers.NewCommand("schedule", scheduleHandler))
+	d.AddHandler(handlers.NewCommand("unschedule", unscheduleHandler))
+	d.AddHandler(handlers.NewCommand("rmJob", unscheduleHandler))
+
+	//	Callbacks
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("jobs:"), jobsPaginationHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("list_projects"), listProjectsHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("project_menu:"), projectMenuHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("sch_m:"), scheduleMenuHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("sch_a:"), scheduleActionHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("sch_c:"), scheduleCreateHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("restart:"), restartHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("deploy:"), deployHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("logs:"), logsHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("status:"), statusHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("stop:"), stopHandler))
+	d.AddHandler(handlers.NewUpdateNewCallbackQuery(callbackquery.Prefix("delete:"), deleteHandler))
+	return nil
 }
